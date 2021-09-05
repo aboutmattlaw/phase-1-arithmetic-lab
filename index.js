c8187c6164c2b2756cdee99d307d539cdee0bb7c
Matt <aboutmattlaw@gmail.com>
2021-09-05 16:27:17 -0400

    commit tree 4d233b3f83e6becde78acad08c4eb1dbe3782d99

--- index.js
+++ index.js
@@ -1,2 +1,54 @@
+// Second Attempt 
 
-// Write your code here
+
+
+
+const num1 = 15.5
+const num2 = 4
+
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 10) + 1;
+
+
+
+const num3 = 12
+const num4 = 3
+const mod = num3 / num4
+const max = Math.max(1, 2, 3, 4, 20);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// First Attempt 
+// // Write your code here
+// let num1 = 31
+// let num2 = 2
+// let multiply =  num1 * num2
+
+// let random = Math.floor(Math.random() * 10) + 1;
+
+// let num3 = 16
+// let num4 = 4
+// let mod =  num3 / num4
+
+// let max = Math.max(1, 2, 3, 4, 20);
